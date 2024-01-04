87ca0fb09aa45b85fb6fd60c6e081209a62cb7a9
ismakr <akroutismail01@gmail.com>
2024-01-04 08:47:50 -0800

    2-1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) == 1:
-    print("{:d} arguments.".format(0))
-else:
-    if len(sys.argv) - 1 == 1:
-        print("{:d} argument:".format(len(sys.argv) - 1))
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 1:
+        print("{:d} arguments.".format(0))
     else:
-        print("{:d} arguments:".format(len(sys.argv) - 1))
-    i = 1
-    n = 1
-    while i <= len(sys.argv) - 1:
-        print("{:d}: {}".format(n, sys.argv[i]))
-        n = n + 1
-        i = i + 1
+        if len(sys.argv) - 1 == 1:
+            print("{:d} argument:".format(len(sys.argv) - 1))
+        else:
+            print("{:d} arguments:".format(len(sys.argv) - 1))
+            i = 1
+            n = 1
+            while i <= len(sys.argv) - 1:
+                print("{:d}: {}".format(n, sys.argv[i]))
+                n = n + 1
+                i = i + 1
