d90a73ceb18389070843cff9ab5b1b53e726a004
ismakr <akroutismail01@gmail.com>
2024-01-04 08:49:34 -0800

    2-0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,8 +9,6 @@ if __name__ == '__main__':
         else:
             print("{:d} arguments:".format(len(sys.argv) - 1))
             i = 1
-            n = 1
             while i <= len(sys.argv) - 1:
-                print("{:d}: {}".format(n, sys.argv[i]))
-                n = n + 1
+                print("{:d}: {}".format(i, sys.argv[i]))
                 i = i + 1
