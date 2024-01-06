afd7e82f78e0ad0fceb6a92e4605cabd3c93163d
ismakr <akroutismail01@gmail.com>
2024-01-06 08:40:47 -0800

    6-0

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    i = 0
+    while i < len(matrix):
+        j = 0
+        while j < len(matrix[i]):
+            if j != len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end=", ")
+            else:
+                print("{:d}".format(matrix[i][j]), end="")
+            j += 1
+        print("")
+        i += 1
