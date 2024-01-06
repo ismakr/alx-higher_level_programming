7b3c1a7e0a1021a4f80c2062855393b281900bc7
ismakr <akroutismail01@gmail.com>
2024-01-06 06:41:22 -0800

    5-1

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,8 +3,7 @@ def no_c(my_string):
     new_string = []
     i = 0
     while i < len(my_string):
-        if my_string[i] == 'c' or my_string[i] == 'C':
-            i += 1
-        new_string.append(my_string[i])
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            new_string.extend(my_string[i])
         i += 1
     return "".join(new_string)
