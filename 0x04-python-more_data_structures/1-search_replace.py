9251fccdd1c719d7d704dfdbb8e707dc04a26fa0
ismakr <akroutismail01@gmail.com>
2024-01-10 12:58:22 -0800

    1-0

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     i = 0
     while i < len(my_list):
-        if my_list[i] == search:
-            my_list[i] = replace
+        if new_list[i] == search:
+            new_list[i] = replace
         i += 1
     return new_list
