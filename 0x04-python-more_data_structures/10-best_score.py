6427cb2fe74692fec1556d03e69903a495ac892f
ismakr <akroutismail01@gmail.com>
2024-01-12 13:45:01 -0800

    10-2

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary.keys()) == 0:
+    if not a_dictionary:
         return None
     li = [(i, j) for i, j in a_dictionary.items()]
     return max(li)[0]
