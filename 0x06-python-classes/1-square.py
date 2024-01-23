6889aec7f1f69f6339bb420dce9c6a1fe787fcc6
ismakr <akroutismail01@gmail.com>
2024-01-23 07:02:48 -0800

    1-0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """class Square that defines a square by:
+        Private instance attribute: size"""
+    def __init__(self, __size=None):
+        self.__size = __size
