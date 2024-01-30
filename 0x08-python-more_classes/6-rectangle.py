c0306d6a04a3bddfb4cb3ae5b4ae13de80ac4404
ismakr <akroutismail01@gmail.com>
2024-01-29 17:13:52 -0800

    6-3

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,8 +4,7 @@
 
 class Rectangle:
     """ class Rectangle that defines a rectangle"""
-
-    count = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Instantiation:
@@ -16,7 +15,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        Rectangle.count += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -79,4 +78,4 @@ class Rectangle:
         """delete an instance"""
         del self
         print("Bye rectangle...")
-        Rectangle.count -= 1
+        Rectangle.number_of_instances -= 1
