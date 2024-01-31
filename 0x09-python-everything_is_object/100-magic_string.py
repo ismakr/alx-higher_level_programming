e695dc1a24b58abba43cecfa8061dd8cf155483c
ismakr <akroutismail01@gmail.com>
2024-01-30 16:24:43 -0800

    100-1

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     magic_string.iter = getattr(magic_string, 'iter', 0) + 1
-    li1 = ["BestSchool" for i in range(magic_string.iter)]
-    return ", ".join(li1)
+    return ", ".join(["BestSchool" for i in range(magic_string.iter)])
