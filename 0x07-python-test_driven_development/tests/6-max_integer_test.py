f081f5f68d91023561505328adc5c22334d794b9
ismakr <akroutismail01@gmail.com>
2024-02-02 15:26:50 -0800

    6-0

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,11 +6,26 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def simple_case(self):
+    def max_end(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def max_big(self):
+        self.assertEqual(max_integer([4, 2, 3, 1]), 4)
+
+    def max_mid(self):
+        self.assertEqual(max_integer([3, 2, 5, 1, 4]), 5)
 
     def noarg_case(self):
         self.assertEqual(max_integer(), None)
+
+    def neg_num(self):
+        self.assertEqual(max_integer([1, 2, 3, -4]), 3)
+
+    def all_neg_num(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def one_el(self):
+        self.assertEqual(max_integer([1]), 1)
 
     def empty_list(self):
         self.assertEqual(max_integer([]), None)
