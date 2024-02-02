a1dbf20252bcb68f7465fd1bf6441b5093ee7bc1
ismakr <akroutismail01@gmail.com>
2024-02-02 15:31:04 -0800

    6-1

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,33 +7,43 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def max_end(self):
+        """max at the end"""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def max_big(self):
+        """max at the bigi"""
         self.assertEqual(max_integer([4, 2, 3, 1]), 4)
 
     def max_mid(self):
+        """max in the mid"""
         self.assertEqual(max_integer([3, 2, 5, 1, 4]), 5)
 
     def noarg_case(self):
+        """no arg case"""
         self.assertEqual(max_integer(), None)
 
     def neg_num(self):
+        """one neg num"""
         self.assertEqual(max_integer([1, 2, 3, -4]), 3)
 
     def all_neg_num(self):
+        """all neg num"""
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
     def one_el(self):
+        """one ele in the list"""
         self.assertEqual(max_integer([1]), 1)
 
     def empty_list(self):
+        """empty list"""
         self.assertEqual(max_integer([]), None)
 
     def float_test(self):
+        """float list"""
         self.assertEqual(max_integer([1.1, 2.3, 5.4]), 5.4)
 
     def string_test(self):
+        """test string"""
         self.assertEqual(max_integer("test"), 't')
 
 
