65d4a7149fb38ff07b775d38bbaf7a1ae5e7702e
ismakr <akroutismail01@gmail.com>
2024-02-05 08:41:03 -0800

    0-1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""object attributes"""
+
+
 def lookup(obj):
+    """function that returns the list of available
+    attributes and methods of an object"""
+
     return dir(obj)
