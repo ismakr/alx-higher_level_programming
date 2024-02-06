e19cde1b989c82ccf5642071e5c4db6501038c88
ismakr <akroutismail01@gmail.com>
2024-02-06 13:19:23 -0800

    4-2

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -7,4 +7,5 @@ def from_json_string(my_str):
     """returns an object (Python data structure)
     represented by a JSON string"""
 
-    return json.loads(my_str)
+    my_str = json.loads(my_str)
+    return my_str
