0a09b2651496283cbd5a5f416c70079f66cdc696
ismakr <akroutismail01@gmail.com>
2024-02-09 06:40:30 -0800

    5-0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,7 @@ class Rectangle(Base):
                 j += 1
             print("")
             i += 1
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
+{self.__width}/{self.__height}"
