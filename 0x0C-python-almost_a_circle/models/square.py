e0e2d533d661a4acd1eac6e3ff6488557d5f7b9c
ismakr <akroutismail01@gmail.com>
2024-02-12 14:18:00 -0800

    -

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,17 @@ class Square(Rectangle):
         """constructor"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """get_width"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """set_width"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """return str"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
