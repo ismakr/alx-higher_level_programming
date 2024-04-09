690f16082e3dbce67c34a5627f9b360788321fe9
ismark <akroutismail01@gmail.com>
2024-04-09 16:47:45 -0700

    6-0

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      let i = 0;
+      while (i < this.height) {
+        console.log(c.repeat(this.width));
+        i += 1;
+      }
+    }
+  }
+}
+module.exports = Square;
