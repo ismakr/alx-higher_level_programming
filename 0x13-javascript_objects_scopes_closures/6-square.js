6c072d11861962c6ef855fb7b9c16d5790ef00cf
ismark <akroutismail01@gmail.com>
2024-04-09 16:52:52 -0700

    6-0

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,13 +7,12 @@ class Square extends Rectangle {
 
   charPrint (c) {
     if (c === undefined) {
-      super.print();
-    } else {
-      let i = 0;
-      while (i < this.height) {
-        console.log(c.repeat(this.width));
-        i += 1;
-      }
+      c = 'X';
+    }
+    let i = 0;
+    while (i < this.height) {
+      console.log(c.repeat(this.width));
+      i += 1;
     }
   }
 }
