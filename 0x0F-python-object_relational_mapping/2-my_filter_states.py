dc0f8cd20298f97a4818e5931061a7370421fd59
ismark <akroutismail01@gmail.com>
2024-04-12 07:33:25 -0700

    2-1

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     # Executing queries: name matches the argument.
-    cur.execute(f"SELECT * FROM states WHERE name = '{sys.argv[4]}'")
+    cur.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
     # fetch and print the result
     rows = cur.fetchall()
     for row in rows:
