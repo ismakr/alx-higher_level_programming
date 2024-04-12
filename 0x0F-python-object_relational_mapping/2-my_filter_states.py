cda2e2a9e544280960b9cd55ba101edd075ae9b6
ismark <akroutismail01@gmail.com>
2024-04-12 07:31:30 -0700

    2-comments

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa"""
+"""displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument."""
+import sys
+import MySQLdb
+
 
 if __name__ == '__main__':
-    import sys
-    import MySQLdb
     # make a connection to the database
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    # Executing queries
+    # Executing queries: name matches the argument.
     cur.execute(f"SELECT * FROM states WHERE name = '{sys.argv[4]}'")
+    # fetch and print the result
     rows = cur.fetchall()
     for row in rows:
         print("{}".format(row))
-    # Clean up
+    #  close all open cursors, and close all open database connections
     cur.close()
     db.close()
