02d4d9092b77f1bc2ee1614e793f4fde92455400
ismark <akroutismail01@gmail.com>
2024-04-12 07:12:14 -0700

    ifmain

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
     import sys
     import MySQLdb
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute(f"SELECT * FROM states WHERE name = {sys.argv[4]}")
     rows = cur.fetchall()
     for row in rows:
         print("{}".format(row))
+    cur.close()
+    db.close()
