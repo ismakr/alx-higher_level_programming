80c614bb352dabe34d77dfb246bacabf24e2c8f7
ismark <akroutismail01@gmail.com>
2024-04-12 07:36:15 -0700

    1-doc

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,9 +5,12 @@ from the database hbtn_0e_0_usa"""
 if __name__ == '__main__':
     import sys
     import MySQLdb
+    # make a connection to the database
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
+    # execute the query: name starting with N
     cur.execute("SELECT * FROM states WHERE name REGEXP '^N[.]*'")
+    # fetch and print the result
     rows = cur.fetchall()
     for row in rows:
         print("{}".format(row))
