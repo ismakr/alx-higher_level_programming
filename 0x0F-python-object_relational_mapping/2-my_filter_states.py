561bbb7e04daf165c948ea2f9cdf8c183079ca04
ismark <akroutismail01@gmail.com>
2024-04-12 07:43:59 -0700

    2-0

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""displays all values in the states table of hbtn_0e_0_usa
+"""module that displays all values in the states table of hbtn_0e_0_usa
 where name matches the argument."""
 
 import sys
