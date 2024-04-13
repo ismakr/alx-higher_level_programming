182c6be0243dad64e17dba4354164cfb968d37dc
ismark <akroutismail01@gmail.com>
2024-04-13 07:14:12 -0700

    6-1

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,6 +9,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """links to the MySQL table states"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
