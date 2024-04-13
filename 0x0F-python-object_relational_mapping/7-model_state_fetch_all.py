21c1337c233f9cbeabe713d378e73edfb22b761c
ismark <akroutismail01@gmail.com>
2024-04-13 08:27:38 -0700

    7-1

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import select, create_engine
