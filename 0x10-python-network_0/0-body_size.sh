043a5372110b398b9ea7888f325fb281968a8e49
ismark <akroutismail01@gmail.com>
2024-04-25 13:07:53 -0700

    0-1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #takes in a URL, sends a request to that URL, and displays
 #the size of the body of the response
-curl -s --head 100.25.45.81 | grep "Content-Length:" | cut -d " " -f2
+curl -s --head "$1" | grep "Content-Length:" | cut -d " " -f2
