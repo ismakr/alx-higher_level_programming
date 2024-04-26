7ccc13ee514cda861cebdc2e53e8804ee907a3dd
ismakr <akroutismail01@gmail.com>
2024-04-26 15:14:19 -0700

    1-3

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,4 +3,4 @@
 import urllib.request
 import sys
 with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers.get('X-Request-Id'))
+    print("{}".format(response.headers.get('X-Request-Id')))
