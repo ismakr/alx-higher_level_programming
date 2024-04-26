1d7084ebd588b71366da6575336336bc2d786477
ismakr <akroutismail01@gmail.com>
2024-04-26 11:53:44 -0700

    0-2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+# Python script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
-from pprint import pprint
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     page = response.read()
     if "utf-8" in str(response.headers.get('Content-Type')):
@@ -8,4 +8,4 @@ with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     else:
         check = ""
     print("Body response:\n\t- type: {}\n\t- content: {}\
-          \n\t- utf8 content: {}".format(type(response.read()), page, check))
+\n\t- utf8 content: {}".format(type(response.read()), page, check))
