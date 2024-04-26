011078f5a3cd7b191080f83530f3b09f57d8bc08
ismakr <akroutismail01@gmail.com>
2024-04-25 17:24:10 -0700

    0-1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #takes in a URL, sends a request to that URL, and displays
 #the size of the body of the response
-curl -sw '%{size_download}\n' -o /dev/null "$1"
+curl -slw '%{size_download}\n' -o /dev/null "$1"
