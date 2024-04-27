da7afb0a106a5511f4953f073444f135fbbb62b1
ismakr <akroutismail01@gmail.com>
2024-04-26 18:15:53 -0700

    5-1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-# Python script that fetches url and return X-Request-Id var
+"""Python script that fetches url and return X-Request-Id var"""
 import requests
 import sys
-r = requests.get(sys.argv[1])
-print(r.headers['X-Request-Id'])
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
