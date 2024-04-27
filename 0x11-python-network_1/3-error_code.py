402abd446981b4f64f52ffcd5f366eac72cde585
ismakr <akroutismail01@gmail.com>
2024-04-26 17:02:08 -0700

    3-0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# sends a request to the URL display the body response
+# and manage urllib.error.HTTPError
+import urllib.request
+import sys
+try:
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.read().decode('utf-8'))
+except urllib.error.HTTPError as e:
+    print("Error code: {}".format(e.code))
