8d6d4ac9d55167a8a4568bfb324573067013a7df
ismakr <akroutismail01@gmail.com>
2024-04-26 18:05:02 -0700

    7-0

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# If the HTTP status code is greater than or equal to 400, print:
+# Error code: followed by the value of the HTTP status code
+import requests
+import sys
+r = requests.get(sys.argv[1])
+if r.status_code >= 400:
+    print("Error code: {}".format(r.status_code))
+else:
+    print(r.text)
