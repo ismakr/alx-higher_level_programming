d2bcd696310d0fc2d9a0fc19712026d021d8c7bd
ismakr <akroutismail01@gmail.com>
2024-04-26 17:21:04 -0700

    4-0

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Python script that fetches https://alx-intranet.hbtn.io/status
+import requests
+r = requests.get('https://alx-intranet.hbtn.io/status')
+print("Body response:\n\t- type: {}\n\t\
+- content: {}".format(type(r.text), r.text))
