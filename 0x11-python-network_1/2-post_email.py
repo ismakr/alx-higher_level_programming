ca99fdd280e3d8448d822b8dbdc81ad7340a7fa2
ismakr <akroutismail01@gmail.com>
2024-04-26 18:13:27 -0700

    2-1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-# sends a POST request to the passed URL with the email as a parameter
+"""sends a POST request to the passed URL with the email as a parameter"""
 import urllib.request
 import sys
-values = {'email': sys.argv[2]}
-url = sys.argv[1]
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    page = response.read().decode('utf-8')
-    print(page)
+
+
+if __name__ == "__main__":
+    values = {'email': sys.argv[2]}
+    url = sys.argv[1]
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        page = response.read().decode('utf-8')
+        print(page)
