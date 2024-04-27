1bc86a39ae6b1b296aa9d50874d159405c0de101
ismakr <akroutismail01@gmail.com>
2024-04-26 18:14:33 -0700

    3-1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-# sends a request to the URL display the body response
+"""sends a request to the URL display the body response"""
 import urllib.request
 import sys
-try:
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.read().decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
