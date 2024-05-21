bfb4dfbfa2322451f5e483f1c0ec523282d0ff92
ismakr <akroutismail01@gmail.com>
2024-05-21 16:30:32 -0700

    1-1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3] + '\n', function (err) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
   if (err) {
     console.error(err);
   }
