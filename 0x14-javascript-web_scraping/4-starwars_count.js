c706a525c29b2fe2a8efd04a4ad0dec271a7dff0
ismakr <akroutismail01@gmail.com>
2024-05-22 16:20:27 -0700

    4-0

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,12 +4,13 @@ const url = process.argv[2];
 request(url, (error, response, body) => {
   if (error) console.log(error);
   const re = JSON.parse(body);
-  const chr = 'https://swapi-api.alx-tools.com/api/people/18/';
-  let j = 0;
+  let ch = 0;
   for (let i = 0; i < re.results.length; i++) {
-    if (re.results[i].characters.includes(chr)) {
-      j++;
+    for (let j = 0; j < re.results[i].characters.length; j++) {
+      if (re.results[i].characters[j].includes('18')) {
+        ch++;
+      }
     }
   }
-  console.log(String(j));
+  console.log(ch);
 });
