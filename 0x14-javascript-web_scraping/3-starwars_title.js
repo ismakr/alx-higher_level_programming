fcc74315c06dae67e8befb4c0ba87c5e34e37de9
ismakr <akroutismail01@gmail.com>
2024-05-21 17:35:42 -0700

    3-0

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+  const re = JSON.parse(body);
+  console.log(re.title);
+});
